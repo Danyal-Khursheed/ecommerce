ce13be3860edcb72afe02275f2d32720e604237a
Danyal Khursheed <danyalkhursheed9@gmail.com>
2023-07-15 13:40:23 +0500

    Files added

---
+++ resources/views/Reusable_components/user/footer.blade.php
@@ -0,0 +1,132 @@
+<!-- Footer -->
+<footer id="footer" class="page-footer font-small  pt-4" style="background:#0a2929;">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">GAINLOE</h6>
+        <p>Hey this is our DBMS project and we are from University of Engineering and Technology Peshawar</p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+        <p>
+          <a href="{{url('Shop/Flowers-Fruits-Basket-Set')}}" target="_blank">Flowers / Fruits Basket Set </a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Dusbin')}}" target="_blank">Dusbin</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Mat')}}" target="_blank">Mat</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Containers')}}" target="_blank">Containers  </a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+          <a href="{{url('about')}}" target="_blank">About Us</a>
+        </p>
+       
+        <p>
+          <a href="{{url('Help')}}" target="_blank">Help</a>
+        </p>
+        <p>
+          <a href="{{url('register')}}" target="_blank">Signup</a>
+        </p>
+          <p>
+          <a href="{{url('login')}}" target="_blank">Login</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i> Uet Peshawar</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> danyalkhursheed9@gmail.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i>Danyal 03498030357</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i>Maaz  03123456789</p>
+          <p>
+          <i class="fas fa-phone mr-3"></i>Hamid 03123456789</p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/rahulvijayam/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li  class="list-inline-item">
+              <a href="https://twitter.com/RahulVijayam" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.instagram.com/rahulvijayam/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/rahulvijayam/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
